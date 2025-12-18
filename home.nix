fd7fb6b3b95d72e3b88191a61e70dd8e0d9d784e
RigelNana <809239189@qq.com>
2025-12-19 00:49:49 +0800

    fix

--- home.nix
+++ home.nix
@@ -65,7 +65,18 @@
       email = "809239189@qq.com";
     };
   };
-
+  programs.ssh.matchBlocks."*" = {
+    forwardAgent = false;
+    addKeysToAgent = "no";
+    compression = false;
+    serverAliveInterval = 0;
+    serverAliveCountMax = 3;
+    hashKnownHosts = false;
+    userKnownHostsFile = "~/.ssh/known_hosts";
+    controlMaster = "no";
+    controlPath = "~/.ssh/master-%r@%n:%p";
+    controlPersist = "no";
+  };
   programs.ssh = {
     enable = true;
     matchBlocks = {
